7725a3dbc45a8315d00b6af729bd546b24b20cc5
Gaurav Raj <gaurav.r@coppermobile.com>
2023-09-15 17:01:43 +0530

    fix

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:mobx_flutter/presentation/common/custom_search.dart';
 import 'package:mobx_flutter/presentation/custom/store_observer.dart';
 import 'package:mobx_flutter/store/weather_store.dart';
+import 'package:mobx_flutter/util_method.dart';
 
 @RoutePage()
 class HomePage extends StatefulWidget {
@@ -26,140 +27,145 @@ class _HomePageState extends State<HomePage> {
     return StoreObserver(builder: (WeatherStore store, context) {
       final data = store.weatherList;
       print("${store.isThemeDark}");
-      return Scaffold(
-          appBar: AppBar(elevation: 0, actions: [
-            IconButton(
-                onPressed: () {
-                  if (store.isThemeDark) {
-                    store.setTheme(false);
-                  } else {
-                    store.setTheme(true);
-                  }
+      return GestureDetector(
+        onTap: () {
+          UtilMethod.hideKeyBoard(context);
+        },
+        child: Scaffold(
+            appBar: AppBar(elevation: 0, actions: [
+              IconButton(
+                  onPressed: () {
+                    if (store.isThemeDark) {
+                      store.setTheme(false);
+                    } else {
+                      store.setTheme(true);
+                    }
 
-                  print("${store.isThemeDark}");
-                },
-                icon: Icon(
-                  store.isThemeDark ? Icons.sunny : Icons.nightlight_rounded,
-                  color: Colors.amber,
-                ))
-          ]),
-          body: Column(children: [
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: searchTextField(store),
-                ),
-                store.weatherList.current == null
-                    ? Container(
-                        child: Text("data not available"),
-                      )
-                    : Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Card(
-                              elevation: 5,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15.0),
-                              ),
-                              child: Container(
-                                child: Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      Text(data.location?.name ?? "",
-                                          style: TextStyle(
-                                              fontSize: 50,
-                                              fontWeight: FontWeight.bold)),
-                                      SizedBox(
-                                        height: 20,
-                                      ),
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text("${data.current?.temperature??""}°C",
-                                              style: TextStyle(
-                                                  fontSize: 30,
-                                                  fontWeight: FontWeight.bold)),
-                                          SizedBox(
-                                            width: 20,
-                                          ),
-                                          data.current?.weatherIcons!= null
-                                              ? Image.network(
-                                                  "${data.current?.weatherIcons?[0]}")
-                                              : Container()
-                                        ],
-                                      ),
-                                      SizedBox(
-                                        height: 20,
-                                      ),
-                                    ],
+                    print("${store.isThemeDark}");
+                  },
+                  icon: Icon(
+                    store.isThemeDark ? Icons.sunny : Icons.nightlight_rounded,
+                    color: Colors.amber,
+                  ))
+            ]),
+            body: Column(children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: searchTextField(store),
+                  ),
+                  store.weatherList.current == null
+                      ? Container(
+                          child: Text("data not available"),
+                        )
+                      : Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Card(
+                                elevation: 5,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                                child: Container(
+                                  child: Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Text(data.location?.name ?? "",
+                                            style: TextStyle(
+                                                fontSize: 50,
+                                                fontWeight: FontWeight.bold)),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text("${data.current?.temperature??""}°C",
+                                                style: TextStyle(
+                                                    fontSize: 30,
+                                                    fontWeight: FontWeight.bold)),
+                                            SizedBox(
+                                              width: 20,
+                                            ),
+                                            data.current?.weatherIcons!= null
+                                                ? Image.network(
+                                                    "${data.current?.weatherIcons?[0]}")
+                                                : Container()
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                      "Humidity ${data.current?.humidity}",
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.bold)),
-                                ),
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                Expanded(
-                                  child: Text(
-                                      "Date and Time ${data.location?.localtime}",
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.bold)),
-                                ),
-                              ],
+                            SizedBox(
+                              height: 20,
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                      "Description ${data.current?.weatherDescriptions}",
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.bold)),
-                                ),
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                Expanded(
-                                  child: Text(
-                                      "Wind Speed ${data.current?.windSpeed}",
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.bold)),
-                                ),
-                              ],
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                        "Humidity ${data.current?.humidity}",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold)),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                        "Date and Time ${data.location?.localtime}",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold)),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-              ],
-            )
-          ]));
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                        "Description ${data.current?.weatherDescriptions}",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold)),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                        "Wind Speed ${data.current?.windSpeed}",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold)),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                ],
+              )
+            ])),
+      );
     });
   }
 
